1b170d54bb1d9876c521eb0b287267d9aa9ba404
Rahul Singh <Rahul.Singh@ezest.local>
2023-09-29 16:08:00 +0530

    revert "update suraj"
    This reverts commit 8e3105a056f009b9a383d5c37f294b2d6d904f4b.

--- hungary.py
+++ hungary.py
@@ -1,8 +1,1 @@
-hungry=input("Are you hungry?")
-if hungry=="yes":
-    print("Eat food")
-    print("Pizza")
-    print("ROLLS")
-    print("suraj")
-else:
-    print("do your work")
+print("i am hungary")
